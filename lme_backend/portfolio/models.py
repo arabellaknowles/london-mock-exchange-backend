fc7ab43f837f84582050851527370f21887c47ab
Oli Le Maire <olilemaire85@gmail.com>
2020-12-10 15:22:14 +0000

    completed creating Portfolio table

--- lme_backend/portfolio/models.py
+++ lme_backend/portfolio/models.py
@@ -6,4 +6,8 @@ from users.models import CustomUser
 class Portfolio(models.Model):
     name = models.CharField(max_length=60)
     balance = models.FloatField()
+    net_earnings = models.FloatField()
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
+    def __str__(self):
+           return self.name

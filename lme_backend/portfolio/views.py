47a5d4093ca98b872b1a5d4d95719d72b2571c21
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-14 12:00:21 +0000

    Added drf nested router dependency, enabling creation of a transaction from a portfolio

--- lme_backend/portfolio/views.py
+++ lme_backend/portfolio/views.py
@@ -11,11 +11,24 @@ class PortfolioViewSet(viewsets.ModelViewSet):
     serializer_class = PortfolioSerializer
 
     def perform_create(self, serializer):
+        print(self.request.user)
         serializer.save(owner=self.request.user)
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
+    # def list(self, request, portfolio_pk=None):
+    #     queryset = Transaction.objects.filter(portfolio=portfolio_pk)
+    #     serializer = TransactionSerializer(queryset, many=True)
+    #     return Response(serializer.data)
+
+    # def retrieve(self, request, pk=None, portfolio_pk=None):
+    #     queryset = Transaction.objects.filter(pk=pk, portfolio=portfolio_pk)
+    #     maildrop = get_object_or_404(queryset, pk=pk)
+    #     serializer = TransactionSerializer(Transaction)
+    #     return Response(serializer.data)
+
     def perform_create(self, serializer):
-        serializer.save()
+        portfolio = Portfolio.objects.get(pk=self.kwargs['portfolio_pk'])
+        serializer.save(portfolio=portfolio)

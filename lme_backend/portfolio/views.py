98ad264b4c0008ef826ace858ee9c70c3ca35469
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-14 12:24:45 +0000

    Deleted comments from views.py in portfolio app

--- lme_backend/portfolio/views.py
+++ lme_backend/portfolio/views.py
@@ -14,23 +14,10 @@ class PortfolioViewSet(viewsets.ModelViewSet):
         serializer.save(owner=self.request.user)
 
 class TransactionViewSet(viewsets.ModelViewSet):
-    # queryset = Transaction.objects.filter(portfolio_id=self.kwargs['portfolio_pk'])
     serializer_class = TransactionSerializer
 
     def get_queryset(self):
         return Transaction.objects.filter(portfolio=self.kwargs['portfolio_pk'])
-
-    # def list(self, request, portfolio_pk=None):
-    #     queryset = Transaction.objects.filter(portfolio=portfolio_pk)
-    #     serializer = TransactionSerializer(queryset, many=True)
-    #     print(serializer.data)
-        # return Response(serializer.data)
-
-    # def retrieve(self, request, pk=None, portfolio_pk=None):
-    #     queryset = Transaction.objects.filter(pk=pk, portfolio=portfolio_pk)
-    #     maildrop = get_object_or_404(queryset, pk=pk)
-    #     serializer = TransactionSerializer(Transaction)
-    #     return Response(serializer.data)
 
     def perform_create(self, serializer):
         portfolio = Portfolio.objects.get(pk=self.kwargs['portfolio_pk'])

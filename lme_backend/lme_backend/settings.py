3c0872c1cc64e4dfbbf66476440da6cfef34127b
Meryl Rastall <merylrastall@gmail.com>
2020-12-15 09:57:51 +0000

    removed session authentification from settings.py file to allow front end to interact with backend using tokens

--- lme_backend/lme_backend/settings.py
+++ lme_backend/lme_backend/settings.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
